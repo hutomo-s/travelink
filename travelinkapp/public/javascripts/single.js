fb2d6ded8ff1c2246bb7e196d4be403b73c5955b
Hutomo Sugianto <hutomo@kudo.co.id>
2016-12-13 09:20:34 +0700

    example for react and webpack integration to play :)

--- travelinkapp/public/javascripts/single.js
+++ travelinkapp/public/javascripts/single.js
@@ -1,1 +1,7 @@
-document.write("It works.");
+import React from 'react';
+import { render } from 'react-dom';
+
+render((
+    <div>
+        <h1>Playframework, React JS, ES 6 and webpack</h1>
+	</div>), document.getElementById("app"));
